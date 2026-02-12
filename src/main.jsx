748498461477ea81c4db048037009bde2f22e995
Toohru <baguilnayrgem@gmail.com>
2026-02-12 16:32:05 +0800

    we free ballin

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,10 @@ import App from './App.jsx';
 import './index.css';
 import { registerSW } from 'virtual:pwa-register';
 
-registerSW({immediate: true});
+registerSW({ immediate: true });
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('Service Worker registered', reg))
-      .catch(err => console.log('Service Worker registration failed', err));
-  });
-}
